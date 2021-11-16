258fa68835e3b3bd4a794111b7b1d61ac2581502
eeyellow <eeyellow@gmail.com>
2021-11-17 01:01:54 +0800

    :sparkles: createElement

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log('hello world !')
+import createElement from './vdom/createElement'
+
+const vApp = createElement('div', {
+    attrs: {
+        id: 'app',
+    },
+    children: []
+})
+
+console.log(vApp)
